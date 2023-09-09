8c3e7288d99d6b39755a4ce8f88d761a72a82196
William He <wwh15@duke.edu>
2023-09-09 13:34:37 -0400

    react to flask

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 
@@ -9,20 +9,28 @@ x = datetime.datetime.now()
  
 # Initializing flask app
 app = Flask(__name__)
+
+@app.route('/add_one', methods=['POST'])
+
+def add_one():
+    data = request.get_json()
+    number = data.get('number', 0)
+    result = number + 1
+    return jsonify({'result': result})
  
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-# Route for seeing a data
-@app.route('/data')
-def get_time():
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# # Route for seeing a data
+# @app.route('/data')
+# def get_time():
  
-    # Returning an api for showing in reactjs
-    return {
-        'Name':"geek",
-        "Age":"22",
-        "Date":x,
-        "programming":"python"
-        }
+#     # Returning an api for showing in reactjs
+#     return {
+#         'Name':"geek",
+#         "Age":"22",
+#         "Date":x,
+#         "programming":"python"
+#         }
  
      
 # Running app
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,47 +2,63 @@ import React from "react"
 
 export default function App() {
 
-  const [data, setData] = React.useState(
-    {
-      name:"",
-      age:0, 
-      date:"",
-      programming:""
+  const [inputValue, setInputValue] = React.useState("")
+  const [result, setResult] = React.useState("")
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/add_one', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ number: parseInt(inputValue) }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setResult(data.result);
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
-  )
+  };
 
-  // Using useEffect for single rendering
-  React.useEffect(() => {
-    // Using fetch to fetch the api from
-    // flask server it will be redirected to proxy
-    fetch("/data").then((res) =>
-        res.json().then((data) => {
-            // Setting a data from api
-            setData({
-                name: data.Name,
-                age: data.Age,
-                date: data.Date,
-                programming: data.programming,
-            });
-        })
-    );
-}, []);
+  
+//   // Using useEffect for single rendering
+//   React.useEffect(() => {
+//     // Using fetch to fetch the api from
+//     // flask server it will be redirected to proxy
+//     fetch("/data").then((res) =>
+//         res.json().then((data) => {
+//             // Setting a data from api
+//             setData({
+//                 name: data.Name,
+//                 age: data.Age,
+//                 date: data.Date,
+//                 programming: data.programming,
+//             });
+//         })
+//     );
+// }, []);
 
   return (
     
     <div className="App">
-        <h1>HACKDUKE</h1>
-        <header className="App-header">
-            <h1>React and flask</h1>
-            {/* Calling a data from setdata for showing */}
-            <p>{data.name}</p>
-            <p>{data.age}</p>
-            <p>{data.date}</p>
-            <p>{data.programming}</p>
-            
+        <h1>Semantic File Search Engine</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            placeholder="Enter a number"
+            value = {inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </form>
 
-        </header>
+        {result != "" && <p>Result: {result}</p>}
+
+
     </div>
   )
 }
