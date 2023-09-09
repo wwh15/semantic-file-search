e9659721511e5db35d86882571d1f73a32b287d2
William He <wwh15@duke.edu>
2023-09-09 14:48:42 -0400

    sqlite

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import sqlite3
 
 
 import datetime
@@ -10,15 +11,27 @@ x = datetime.datetime.now()
 # Initializing flask app
 app = Flask(__name__)
 
-@app.route('/process_string', methods=['POST'])
 
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/process_string', methods=['POST'])
 def process_string():
     data = request.get_json()
     input = data.get('inputString', '')
+
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
     
+    return jsonify({'result': posts[1][1]})
     
 
-    return jsonify({'result': input})
+    # return jsonify({'result': input})
+    
  
 # cors = CORS(app)
 # app.config['CORS_HEADERS'] = 'Content-Type'
