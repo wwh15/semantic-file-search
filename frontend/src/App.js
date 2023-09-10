655c97dfac912c3c1e5b84b573cc9e77eabfb401
Steven Peng <steven.peng@duke.edu>
2023-09-09 23:43:45 -0400

    comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,59 @@
 import React from "react"
 
+
+// defines React component App as default export from module
 export default function App() {
 
-  const [inputValue, setInputValue] = React.useState("")
-  const [result, setResult] = React.useState([])
+  const [inputValue, setInputValue] = React.useState("") 
+  const [result, setResult] = React.useState([]) 
 
-  const resultArr = result.map(item => <p>{item}</p>)
+  // resultArr is updated when result is updated
+  const resultArr = result.map(item => <p>{item}</p>) // creates array of paragraph elements, each item in result is its own paragraph
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // async function to handle form submission
+  const handleSubmit = async (e) => { 
+    e.preventDefault(); // prevents empty form submissions (cause page to refresh)
+
     try {
-      const response = await fetch('/process_string', {
-        method: 'POST',
+      const response = await fetch('/process_string', { // fetches data from server, awaits until fetched
+        method: 'POST', // sets HTTP method to POST
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json', // content sent is JSON formatted
         },
-        body: JSON.stringify({ inputString: inputValue }),
+        body: JSON.stringify({ inputString: inputValue }), // sends current value of inputValue
       });
 
-      if (response.ok) {
-        const data = await response.json();
-        setResult(data.result);
+      if (response.ok) { // checks if HTTP response is 200-299
+        const data = await response.json(); // parses JSON response from server, assigns to data, awaits till json parsed
+        setResult(data.output); // updates result with output from parsed response
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error:', error); // logs errors to console
     }
   };
 
 
+  // JSX return statement, defines component (App) render
   return (
-    
-    <div className="App">
-        <h1>Semantic File Search Engine</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter a search term"
+    <div className = "App"> 
+        {/*renders heading for component*/}
+        <h1> Semantic File Search Engine </h1> 
+
+        {/*creates form element onSubmit, when form is submitted handleSubmit called*/}
+        <form onSubmit = {handleSubmit}> 
+          {/*renders input search bar, value bound to inputValue*/}
+          <input 
+            type = "text"
+            placeholder = "Enter a search term"
             value = {inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            onChange = {(e) => setInputValue(e.target.value)} // updates inputValue when event object (e) {input} is changed
           />
-          <button type="submit">Process</button>
+          {/*renders button that submits form*/}
+          <button type = "submit"> Process </button>
         </form>
 
-
-        {result != "" && <p>Result: {resultArr}</p>}
-
-
+        {/*renders paragraph containing results only result is not empty*/}
+        {result != "" && <p> Result: {resultArr} </p>} 
     </div>
   )
 }
