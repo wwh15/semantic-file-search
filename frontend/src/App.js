a9bdbe3e6336d2a0cd7e3160fe8bc77d3a8abaf4
Peter Liu <pl217@duke.edu>
2023-09-10 16:45:29 -0400

    added stars, reviews, and review count

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,20 +7,55 @@ export default function App() {
   const [matches, setMatches] = React.useState(0)
   const [result, setResult] = React.useState([]) 
 
+
+  function Star({ percentage }) {
+    return (
+      <div className="star">
+        <span className="star-empty">☆</span>
+        <div className="star-fill" style={{ width: `${percentage}%` }}>
+          ★
+        </div>
+      </div>
+    );
+  }
   // resultArr is updated when result is updated
-  const resultArr = result.map(item =>
-     <div className="result">
-      <h1 className="result-name">
-        {item.Name} 
-      </h1>
-      <p className="result-description">
-        {item.Description}
-      </p>
-      <p>
-        Rating: {item.Rating}
-      </p>
-     </div>
-     ) // creates array of paragraph elements, each item in result is its own paragraph
+  function Rating({ value }) {
+    return (
+      <div className="rating">
+        {[...Array(5)].map((_, index) => {
+          const fillPercentage = Math.min(Math.max((value - index) * 100, 0), 100);
+          return <Star key={index} percentage={fillPercentage} />;
+        })}
+      </div>
+    );
+  }
+
+  const resultArr = result.map(item => {
+    const roundedRating = Math.round(item.Rating * 100) / 100;
+    const isRatingValid = !isNaN(roundedRating) && item.Rating !== null;
+    
+    return (
+        <div className="result">
+            <h1 className="result-name">
+                {item.Name} 
+            </h1>
+            <p className="result-rating">
+                {isRatingValid 
+                    ? <>
+                        {roundedRating} <Rating value={roundedRating} />  reviewed by {item.Rating_Volume}
+                      </>
+                    : "No reviews"
+                }
+            </p>
+            <p className="result-description">
+                {item.Description}
+            </p>
+            <p className="result-genre">
+                Genre: {item.Genre}
+            </p>
+        </div>
+    );
+});// creates array of paragraph elements, each item in result is its own paragraph
 
   // async function to handle form submission
   const handleSubmit = async (e) => { 
@@ -47,32 +82,29 @@ export default function App() {
 
   // JSX return statement, defines component (App) render
   return (
-    <div className = "App"> 
-        {/*renders heading for component*/}
-        <h1> Semantic Podcast Search Engine </h1> 
-
-        {/*creates form element onSubmit, when form is submitted handleSubmit called*/}
-        <form onSubmit = {handleSubmit}> 
-          {/*renders input search bar, value bound to inputValue*/}
-          <input 
-            type = "text"
-            placeholder = "Enter a search term"
-            value = {inputValue}
-            onChange = {(e) => setInputValue(e.target.value)} // updates inputValue when event object (e) {input} is changed
-          />
-          {/* renders number input for how many entries on screen */}
-          <input
-            type = "number"
-            placeholder = "How many entries?"
-            value = {matches}
-            onChange={(e) => setMatches(e.target.value)}
-          />
-          {/*renders button that submits form*/}
-          <button type = "submit"> Process </button>
-        </form>
-
-        {/*renders paragraph containing results only result is not empty*/}
-        {result != "" && <p> Result: {resultArr} </p>} 
+    <div className="App"> 
+      <h1> Semantic Podcast Search Engine </h1> 
+  
+      <form onSubmit={handleSubmit}> 
+        <input 
+          type="text"
+          placeholder="Enter a search term"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+  
+        <input
+          type="number"
+          placeholder="How many entries?"
+          value={matches}
+          onChange={(e) => setMatches(e.target.value)}
+        />
+  
+        <button type="submit"> Search </button>
+      </form>
+  
+      {/* Renders the results only if result array has items */}
+      {result.length > 0 && resultArr} 
     </div>
   )
 }
